249d9cb2996046f04ab7232d627f96309f5ae2fb
rudkik <66820365+rudkik@users.noreply.github.com>
2022-02-16 08:08:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
-
-
 <?php
 
 require __DIR__. '/vendor/autoload.php';
@@ -9,3 +7,15 @@ $clientSecret = '';
 $redirectUri = '';
 
 $apiClient = new \AmoCRM\Client\AmoCRMApiClient($clientId, $clientSecret, $redirectUri);
+
+?>
+
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Пример веб-страницы</title>
+</head>
+<body>
+
+</body>
+</html>
